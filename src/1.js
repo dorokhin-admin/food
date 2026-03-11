d488ff2d63791a72b2d05b72e36a011116809bba
dorokhin-admin <dorokhin.ilyaa@gmail.com>
2026-03-11 21:17:10 +0500

    next ts

--- src/1.js
+++ src/1.js
@@ -1,14 +1,14 @@
-var Product1 = { id: 1, name: 'anume', price: 22 };
-var Product2 = { id: 2, name: 'asdasd', price: 11, description: 'sadadsa' };
-var Product3 = { id: 3, name: 'asdasd', price: 11, description: 'sadadsa' };
-function getProductDescription(product) {
-    if (product.description) {
-        return product.description;
+var myProducts = [
+    { id: 1, name: "Phone", price: 1000, discount: 10 },
+    { id: 2, name: "Laptop", price: 2000 },
+    { id: 3, name: "Tablet", price: 500, discount: 20 }
+];
+function getFinalPrice(product) {
+    if (product.discount !== undefined) {
+        return product.price - product.price * product.discount / 100;
     }
     else {
-        return 'otsutstvuet';
+        return product.price;
     }
 }
-getProductDescription(Product2);
-console.log(getProductDescription(Product1));
-console.log('log');
+console.log(getFinalPrice(myProducts[0]));
--- src/1.ts
+++ src/1.ts
@@ -1,22 +1,24 @@
 type Product = {
     id: number
     name: string
-    description?: string
     price: number
+    discount?: number
 }
 
-const Product1 = {id: 1, name: 'anume', price: 22}
-const Product2 = {id: 2, name: 'asdasd', price: 11, description:'sadadsa'}
-const Product3 = {id: 3 , name: 'asdasd', price: 11, description:'sadadsa'}
+const myProducts: Product[] = [
+    { id: 1, name: "Phone", price: 1000, discount: 10 },
+    { id: 2, name: "Laptop", price: 2000 },
+    { id: 3, name: "Tablet", price: 500, discount: 20 }
+]
 
-function getProductDescription(product: Product){
-    if(product.description){
-        return product.description
-    }else{
-        return 'otsutstvuet'
-    }
+function getFinalPrice(product: Product): number{
+
+         if(product.discount !== undefined){
+            return product.price - product.price * product.discount / 100
+        }else{
+            return product.price
+        }
+
 }
 
-getProductDescription(Product2)
-console.log(getProductDescription(Product1))
-console.log('log')
+console.log(getFinalPrice(myProducts[0]))
